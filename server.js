cf88dab2164faa8fa555470ab4a5be52b19e3479
Govind Sharma <goviiind45@gmail.com>
2020-10-26 22:40:22 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+require("dotenv").config()
+
+const fs = require("fs")
+
 const express = require("express");
 
 const ejs = require("ejs");
@@ -6,7 +10,58 @@ const path = require("path");
 
 const expressLayout = require("express-ejs-layouts");
 
+const session  = require("express-session");
+
+
+const mongoose = require("mongoose");
+
+const flash = require("express-flash");
+
+const MongoDbStore = require("connect-mongo")(session);
+
+
 const app = express();
+
+
+//Database Connection
+
+const url = 'mongodb://localhost:27017/Pizza';
+mongoose.connect(url ,{useCreateIndex:true , useNewUrlParser:true,useFindAndModify:true,useUnifiedTopology:true});
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log('Database Connected');
+}).catch(err =>{
+    console.log('Connection Failed')
+});
+
+//Session Store
+
+let mongoStore = new MongoDbStore({
+    mongooseConnection : connection,
+    collection : "sessions"
+})
+
+//Session Config
+app.use(session({
+
+    secret : process.env.COOKIE_SECRET,
+    resave : false,
+    store : mongoStore,
+    saveUninitialized : false,
+    cookie : {maxAge : 1000 * 60 * 60 * 24} //24 Hours
+}))
+    
+//Assets
+app.use(flash())
+app.use(express.json())
+
+//Global Middleware for session to be available everywhere
+
+app.use((req , res , next) => {
+    res.locals.session = req.session
+    next()  //without next() http request will not complete
+})
+
 
 const PORT = process.env.PORT || 3000 ;
 
@@ -22,21 +77,7 @@ app.set('views',path.join(__dirname, '/resources/views'));
 app.set('view engine','ejs');
 
 
-app.get("/", (req,res) => {
-    res.render("home")
-})
-
-app.get("/cart", (req,res) => {
-    res.render("./customer/cart");
-})
-
-app.get("/register", (req , res) =>{
-    res.render("./auth/register")
-})
-
-app.get("/login", (req , res) =>{
-    res.render("./auth/login")
-})
+require('./routes/web')(app);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 let mix = require('laravel-mix');
 
+mix.webpackConfig({ node: { fs: 'empty' }});
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,6 +14,7 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css');
+
 
 // Full API
 // mix.js(src, output);
